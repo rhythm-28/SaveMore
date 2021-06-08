1e822b9ea735ce29d79c3145d5d3102ec9ccc310
RidamGarg <67867319+RidamGarg@users.noreply.github.com>
2021-06-08 21:43:24 +0530

    Update index.js

--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -1,8 +1,8 @@
 import Login from './user/auth/login';
-import RenderTextInput from './input';
+import {RenderTextInput,RenderTextSelect} from './input';
 import Signup from './user/auth/signup';
 import adminLogin from './admin/auth/login';
 import adminSignup from './admin/auth/signup';
 import UserForm from './user/auth/form';
 import Home from "./user/home/home.js";
-export { Login, RenderTextInput, Signup, adminLogin, adminSignup, UserForm,Home };
+export { Login, RenderTextInput, Signup, adminLogin, adminSignup, UserForm,Home,RenderTextSelect };
