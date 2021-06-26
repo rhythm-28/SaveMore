e152674f8b5c121f28cdb4ded3525ebf3a5ee6c1
Rhythm Bhatia <rhythmbhatiarb28@gmail.com>
2021-06-26 21:21:43 +0530

    Deployed

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,14 @@ import './stylesheets/index.css';
 import App from './Components/App';
 import { Provider } from 'react-redux';
 import store from './store';
-
+import { HashRouter } from 'react-router-dom';
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
-      {' '}
-      <App />
+      <HashRouter>
+        {' '}
+        <App />
+      </HashRouter>
     </React.StrictMode>
   </Provider>,
   document.getElementById('root')
